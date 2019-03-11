ce319d8d39040018796008de159474b776caf991
Abhishek Kumar <akpal.abhishek@gmail.com>
2019-03-11 12:22:17 +0530

    added dockere-react folder

---
+++ docker-react/build/precache-manifest.6f2790c478bf3e98a85caa006981ccf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a2006e9e48837f82a4f2",
+    "url": "/static/js/main.a2006e9e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "a2006e9e48837f82a4f2",
+    "url": "/static/css/main.30e3cef4.chunk.css"
+  },
+  {
+    "revision": "7fa0a663d8dd4a1f4a8c0d5c98678b6a",
+    "url": "/index.html"
+  }
+];
